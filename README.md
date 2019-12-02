e3863863395d821fdf8ca36f2a1e3e0eaa8f13bc
Michael Standen <screaminghawk@gmail.com>
2019-12-02 13:21:37 +1300

    Add notify webhook

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# serverless-civ-webhook
+# Serverless Civ Webhook
+
 A serverless webhook to notify when it is your turn in a Civilization VI play by cloud game
+
+## Set up
+
+Install [serverless](https://serverless.com/framework/docs/providers/aws/guide/installation/).
+Configure your [AWS credentials](https://serverless.com/framework/docs/providers/aws/guide/credentials/).
+Create a [discord bot](https://discordjs.guide/preparations/setting-up-a-bot-application.html##creating-your-bot).
+
+Copy the example environment variables file.
+
+```sh
+cp env.example .env
+```
+
+Modify the values in the `.env` file with your values where:
+* `DISCORD_TOKEN` is your discord bot's token
+* `DEFAULT_CHANNEL` is the default channel id for your notifications
+
+## Deployment
+
+```sh
+serverless deploy --aws-profile default
+```
+
+### Test
+
+```sh
+sls invoke -f notify --aws-profile default --path test/civ.example.json
+```
+
+### Logs
+
+```sh
+sls logs -f notify --aws-profile default
+```
+
+## Test locally
+
+Replace `XXX` with your env values.
+
+```sh
+sls invoke local -f notify --path test/civ.example.json -e discordToken=XXX -e defaultChannel=XXX
+```
---
+++ src/notify.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const discordjs = require('discord.js')
+const discord = new discordjs.Client()
+
+// Map Civ game name to Discord channel Id
+const games = {
+	"Lovers": "478505976711086082",
+}
+
+// Map Civ player name to Discord user Id
+const players = {
+	"MilkyTaste": "172630139878768640",
+}
+
+module.exports.notify = async event => {
+	// Validate request
+	console.debug(event.body)
+	const data = JSON.parse(event.body)
+	if (!data || !data.value1 || !data.value2){
+		console.error('Invalid notify request')
+		return {
+			statusCode: 400,
+		}
+	}
+	let gameName = data.value1
+	let civPlayer = data.value2
+	console.info(`Notifying ${civPlayer} of turn in game ${gameName}`)
+
+	// Login to discord
+	try {
+		await discord.login(process.env.discordToken)
+	} catch (err){
+		console.error(`Discord login failed: ${err}`)
+		console.log(`Token: ${process.env.discordToken}`)
+		return {
+			statusCode: 500,
+		}
+	}
+
+	// Get channel
+	let channelId = games[gameName] || process.env.defaultChannel
+	const channel = discord.channels.get(channelId)
+	if (!channel){
+		console.error(`Invalid channel ${channelId}, using default`)
+		channel = discord.channels.get(process.env.defaultChannel)
+		if (!channel){
+			// Fail default channel invalid
+			console.error(`Invalid default channel ${process.env.defaultChannel}`)
+			return {
+				statusCode: 400,
+			}
+		}
+	}
+
+	// Get player
+	let player = players[civPlayer]
+	if (!player){
+		console.warn(`Unable to find player with civName ${civPlayer}`)
+		player = civPlayer
+	} else {
+		player = `<@${player}>`
+	}
+
+	// Send it
+	console.debug('Sending notification')
+	await channel.send(`Hey ${player}! It's your turn in Civ 6 game ${gameName}`)
+	console.debug('Notification sent')
+
+	// Logout of discord
+	try {
+		await discord.destroy()
+	} catch (err){
+		console.error(`Discord logout failed: ${err}`)
+		// Fail over, still a success
+	}
+
+	return {
+		statusCode: 200,
+	}
+}
